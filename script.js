4e6ed300516f185c97dc62f8aec9f936c4e3d831
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-11-02 13:10:34 -0400

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var guest_Name = document.querySelector('.name');
 var btn = document.querySelector('.enter');
+var msg = document.querySelector('.message');
 
 var guestName = [];
 for (var i = 0; i < 12; i++) {
@@ -9,19 +10,24 @@ var arrayImage = ['img/alia.png', 'img/heeman.png', 'img/ben.png', 'img/girl.png
 
 function clickButton() {
     if (guestName.length !== 0) {
-        var guest = "";
+        var guest = '';
         var currentPic = arrayImage.shift();
         var currentName = guestName.shift();
         guest += `<img src="${currentPic}" style="width: 18vw; height: 40vh;"/>  
                     <p>${currentName} </p>
                     `;
-        console.log(guest);
-        guest_Name.innerHTML = guest + '';
+        guest_Name.innerHTML = guest;
+        if ((guestName.length % 3) === 0) {
+            msg.innerHTML += '*** Congratulations!!! You are the lukiest winner to get 25% off tonight. ***';
+        }
+        else {
+            msg.innerHTML = '';
+        }
     } else {
-        guest_Name.innerHTML = "<br/>*** There is no more guest left. ***<br/>Great Job!!!";
+        msg.innerHTML = '';
+        guest_Name.innerHTML = "<br/>*** There is no more guest left. All guests are seated. ***<br/>Great Job!!!";
     }
 }
-
 btn.addEventListener('click', () => {
     clickButton();
 });
